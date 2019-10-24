7b0783d01cb60af72c7a8ea266bf4ab609107fcc
tachiDhon <53515928+tachiDhon@users.noreply.github.com>
2019-10-24 15:39:13 +0200

    3rd commit with all the functions

--- index.php
+++ index.php
@@ -1,54 +1,29 @@
 <?php
 
-//require("push_swap.php");
+require_once "push_swap.php";
 
 function list_Swap($a, $count) {
    
    $la = $a;
    $lb = [22, 33, 44];
-   // for($i=0; $i<$count; $i++) {
-   //    array_push($la, $a[$i]);     
-   // }
+
    $new_la = array_shift($la);
-   print_r($la);
-   // $la = sa($la); 
+   //print_r($la);
+
+   //$la = sa($la); 
    // $lb = sb($lb);
 
-   sc($la, $lb); //on utilise soit sa(), sb() ou sc(), ainsi on les mettre en parametre selon nos essaie.
+   //sc($la, $lb); //on utilise soit sa(), sb() ou sc(), ainsi on les mettre en parametre selon nos essaie.
+   //pa($la, $lb);
+   //pb($la, $lb);
+   //ra($la);
+   //rb($lb);
+   rr($la, $lb);
+   //rra($la);
+   //rrb($lb);
+   //rrr($la, $lb);
 }
 list_swap($argv, $argc);
 
-function sa(&$la) {
 
-   if(!empty($la)) {
-      $tmp = $la[0];
-      $la[0] = $la[1];
-      $la[1] = $tmp;
-      
-      print_r($la);
-   }
-  //return $la;
-}
 
-function sb(&$lb) {
-   if(!empty($lb)) {
-      $tmp = $lb[0];
-      $lb[0] = $lb[1];
-      $lb[1] = $tmp;
-
-      print_r($lb);
-   }
-   //return $lb;
-}
-
-function sc(&$la, &$lb) {
-   if(isset($la[1], $lb[1])) {
-      sa($la);
-      sb($lb);
-   }
-}
-
-function pa($lb, $la) {
-
-   if(isset($lb[]))
-}
--- push_swap.php
+++ push_swap.php
@@ -1,5 +1,89 @@
 <?php
 
-// function sb($lb) {
+function sa(&$la) {
+
+   if(!empty($la)) {
+      $tmp = $la[0];
+      $la[0] = $la[1];
+      $la[1] = $tmp;
+      echo 'sa ';
+      print_r($la);
+   }
+  //return $la;
+}
+
+function sb(&$lb) {
+   if(!empty($lb)) {
+      $tmp = $lb[0];
+      $lb[0] = $lb[1];
+      $lb[1] = $tmp;
+      echo 'sb ';
+      print_r($lb);
+   }
+   //return $lb;
+}
+
+function sc(&$la, &$lb) {
+   if(isset($la[1], $lb[1])) {
+      sa($la);
+      sb($lb);
+   }
+}
+
+function pa(&$la, &$lb) {
+
+   if(!empty($lb)){
+      $first = array_shift($lb);
+      array_unshift($la, $first);
+      print_r($la);
+      //print_r($lb);
+   }
+}
+
+function pb(&$la, &$lb) {
+
+   if(!empty($la)) {
+      $first = array_shift($la);
+      array_unshift($lb, $first);
+      print_r($lb);
+      //print_r($la);
+   }
+}
+
+function ra($la) {
+   $second = array_shift($la);
+   array_push($la, $second);
+   print_r($la);
    
-// }
+   return $la;
+}
+
+function rb($lb) {
+   $third = array_shift($lb);
+   array_push($lb, $third);
+   print_r($lb);
+
+   return $lb;
+}
+
+function rr(&$la, &$lb) {
+   ra($la);
+   rb($lb);
+}
+
+function rra(&$la) {
+   $fifth = array_pop($la);
+   array_unshift($la, $fifth);
+   print_r($la);
+}
+
+function rrb(&$lb) {
+   $fifth = array_pop($lb);
+   array_unshift($lb, $fifth);
+   print_r($lb);
+}
+
+function rrr(&$la, &$lb) {
+   rra($la);
+   rrb($lb);
+}
